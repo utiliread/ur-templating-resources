cf719100e8f9db920c8cc3ce443723f4269ea5ba
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2024-01-18 15:49:10 +0100

    Preliminary support for changing items array

--- src/select2-select.ts
+++ src/select2-select.ts
@@ -1,147 +1,170 @@
-import 'select2';
+import "select2";
 
-import { BindingEngine, Disposable, TaskQueue, autoinject, bindable, bindingMode } from 'aurelia-framework';
-import { DataFormat, Options } from 'select2';
+import {
+  BindingEngine,
+  Disposable,
+  TaskQueue,
+  autoinject,
+  bindable,
+  bindingMode,
+} from "aurelia-framework";
+import { DataFormat, Options } from "select2";
 
-import $ from 'jquery';
+import $ from "jquery";
 
 @autoinject()
 export class Select2SelectCustomElement {
-    private bindingContext: any;
+  private bindingContext: any;
 
-    @bindable()
-    name?: string;
-    @bindable({ defaultBindingMode: bindingMode.twoWay })
-    selected: string[] = [];
-    @bindable()
-    items: DataFormat[] = [];
-    @bindable()
-    minimumInputLength: string | number = 0;
-    @bindable()
-    placeholder = '';
-    @bindable()
-    disabled: boolean;
-    @bindable()
-    autofocus: boolean;
-    @bindable()
-    theme = "default";
+  @bindable()
+  name?: string;
+  @bindable({ defaultBindingMode: bindingMode.twoWay })
+  selected: string[] = [];
+  @bindable()
+  items: DataFormat[] = [];
+  @bindable()
+  minimumInputLength: string | number = 0;
+  @bindable()
+  placeholder = "";
+  @bindable()
+  disabled: boolean;
+  @bindable()
+  autofocus: boolean;
+  @bindable()
+  theme = "default";
 
-    @bindable()
-    query?: (q: string, pageNumber?: number) => Promise<{ id: string; text: string }[] | { id: string; text: string }[]>;
+  @bindable()
+  query?: (
+    q: string,
+    pageNumber?: number
+  ) => Promise<{ id: string; text: string }[] | { id: string; text: string }[]>;
 
-    selectElement!: HTMLSelectElement;
-    private itemsCollectionSubscription?: Disposable;
+  selectElement!: HTMLSelectElement;
+  private itemsCollectionSubscription?: Disposable;
 
-    constructor(private taskQueue: TaskQueue, private bindingEngine: BindingEngine) {
-        this.itemsCollectionChanged = this.itemsCollectionChanged.bind(this);
+  constructor(
+    private taskQueue: TaskQueue,
+    private bindingEngine: BindingEngine
+  ) {
+    this.itemsCollectionChanged = this.itemsCollectionChanged.bind(this);
+  }
+
+  bind(bindingContext: any) {
+    this.bindingContext = bindingContext;
+
+    if (this.items) {
+      this.itemsCollectionSubscription = this.bindingEngine
+        .collectionObserver(this.items)
+        .subscribe(this.itemsCollectionChanged);
     }
+  }
 
-    bind(bindingContext: any) {
-        this.bindingContext = bindingContext;
+  attached() {
+    const options: Options<any, any> = {
+      minimumInputLength: Number(this.minimumInputLength),
+      theme: this.theme,
+    };
 
-        if (this.items) {
-            this.itemsCollectionSubscription = this.bindingEngine.collectionObserver(this.items).subscribe(this.itemsCollectionChanged);
-        }
-    }
+    if (this.items) {
+      options.data = this.items;
+    } else if (this.query) {
+      const query = this.query;
 
-    attached() {
-        const options: Options<any, any> = {
-            minimumInputLength: Number(this.minimumInputLength),
-            theme: this.theme
-        };
+      options.ajax = {
+        transport: (settings, success, failure) => {
+          const data: any = settings.data;
 
-        if (this.items) {
-            options.data = this.items;
-        }
-        else if (this.query) {
-            const query = this.query;
-            
-            options.ajax = {
-                transport: (settings, success, failure) => {
-                    const data: any = settings.data;
+          if (!success || !failure) {
+            throw Error();
+          }
 
-                    if (!success || !failure) {
-                        throw Error();
-                    }
-
-                    Promise.resolve(query.call(this.bindingContext, data.q, data.page || 1))
-                        .then((result: LookupObjectResult | DataFormat[]) => {
-                            if (Array.isArray(result)) {
-                                success({
-                                    results: result
-                                });
-                            }
-                            else if (result) {
-                                success({
-                                    results: result.items,
-                                    pagination: {
-                                        more: !!result.more
-                                    }
-                                });
-                            }
-                            else {
-                                success({
-                                    results: []
-                                });
-                            }
-                        })
-                        .catch(() => {
-                            failure();
-                        });
-
-                    return undefined;
-                },
-                delay: 250,
-                cache: true
-            };
-        }
-
-        $(this.selectElement)
-            .select2(options)
-            .val(this.selected).trigger('change')
-            .prop("disabled", this.disabled)
-            .on('change', event => {
-                // Don't propagate endlessly
-                if (event.originalEvent) {
-                    return;
-                }
-
-                // Dispatch to raw select within the custom element.
-                let notice = new Event('change', {
-                    bubbles: false
+          Promise.resolve(
+            query.call(this.bindingContext, data.q, data.page || 1)
+          )
+            .then((result: LookupObjectResult | DataFormat[]) => {
+              if (Array.isArray(result)) {
+                success({
+                  results: result,
                 });
-                this.selectElement.dispatchEvent(notice);
+              } else if (result) {
+                success({
+                  results: result.items,
+                  pagination: {
+                    more: !!result.more,
+                  },
+                });
+              } else {
+                success({
+                  results: [],
+                });
+              }
+            })
+            .catch(() => {
+              failure();
             });
-        
-        if (this.autofocus) {
-            // Queue the open until after the control is displayed to ensure that it opens below the select control
-            this.taskQueue.queueTask(() => $(this.selectElement).select2('open'));
+
+          return undefined;
+        },
+        delay: 250,
+        cache: true,
+      };
+    }
+
+    $(this.selectElement)
+      .select2(options)
+      .val(this.selected)
+      .trigger("change")
+      .prop("disabled", this.disabled)
+      .on("change", (event) => {
+        // Don't propagate endlessly
+        if (event.originalEvent) {
+          return;
         }
-    }
 
-    detached() {
-        $(this.selectElement).select2('destroy');
+        // Dispatch to raw select within the custom element.
+        const notice = new Event("change", {
+          bubbles: false,
+        });
+        this.selectElement.dispatchEvent(notice);
+      });
 
-        if (this.itemsCollectionSubscription) {
-            this.itemsCollectionSubscription.dispose();
-            this.itemsCollectionSubscription = undefined;
-        }
+    if (this.autofocus) {
+      // Queue the open until after the control is displayed to ensure that it opens below the select control
+      this.taskQueue.queueTask(() => $(this.selectElement).select2("open"));
     }
+  }
 
-    itemsCollectionChanged() {
-        $(this.selectElement).trigger('change');
-    }
+  detached() {
+    $(this.selectElement).select2("destroy");
 
-    selectedChanged() {
-        $(this.selectElement).val(this.selected).trigger('change');
+    if (this.itemsCollectionSubscription) {
+      this.itemsCollectionSubscription.dispose();
+      this.itemsCollectionSubscription = undefined;
     }
+  }
 
-    disabledChanged() {
-        $(this.selectElement).prop("disabled", this.disabled);
-    }
+  itemsChanged() {
+    this.itemsCollectionSubscription?.dispose();
+    $(this.selectElement).trigger("change");
+    this.itemsCollectionSubscription = this.bindingEngine
+      .collectionObserver(this.items)
+      .subscribe(this.itemsCollectionChanged);
+  }
+
+  itemsCollectionChanged() {
+    $(this.selectElement).trigger("change");
+  }
+
+  selectedChanged() {
+    $(this.selectElement).val(this.selected).trigger("change");
+  }
+
+  disabledChanged() {
+    $(this.selectElement).prop("disabled", this.disabled);
+  }
 }
 
 interface LookupObjectResult {
-    items: DataFormat[];
-    more: boolean;
+  items: DataFormat[];
+  more: boolean;
 }
